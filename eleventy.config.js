48ad88c14ab51fd7e3e844c8059d05e6dce33582
carlknutson <cknutson12@gmail.com>
2025-02-22 23:19:57 -0600

    fix config used by rss feed page

--- eleventy.config.js
+++ eleventy.config.js
@@ -63,11 +63,12 @@ export default async function(eleventyConfig) {
 		},
 		metadata: {
 			language: "en",
-			title: "Blog Title",
-			subtitle: "This is a longer description about your blog.",
-			base: "https://example.com/",
+			title: "Freeze the Pint",
+			subtitle: "Freeze the Pint is your go-to destination for homemade ice cream recipes, tips, and flavor experiments. Whether you're a beginner or an ice cream enthusiast, you'll find a collection of unique, tested recipes and insights into the art and science of ice cream making. Follow along for new flavors, creative techniques, and the occasional kitchen experiment—because the best ice cream is the one you make yourself!",
+			base: "https://freezethepint.com/",
 			author: {
-				name: "Your Name"
+				name: "Carl",
+				email: "carl@freezethepint.com"
 			}
 		}
 	});
